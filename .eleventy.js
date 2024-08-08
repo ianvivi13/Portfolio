c5ad2bbe86e64ebc0ca48daf5c0c3b3631879be6
ianvivi13 <ianvivi13@gmail.com>
2024-08-08 19:36:22 -0400

    Initial commit. Home page mostly complete

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("src/_styles/**/*.css");
+    eleventyConfig.addPassthroughCopy("src/_scripts/**/*.js");
+    eleventyConfig.addPassthroughCopy("src/_images/**/*.jpg");
+    eleventyConfig.addPassthroughCopy("src/_images/**/*.png");
+
+    return {
+        dir: {
+            input: "src",
+            includes: "_includes",
+            layouts: "_layouts"
+        }
+    }
+};
