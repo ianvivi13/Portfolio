5a5d006cf94fcaf58700fe3699651c33bb3f9f27
ianvivi13 <ianvivi13@gmail.com>
2024-08-08 20:18:51 -0400

    Fixed compile stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("src/_styles/**/*.css");
-    eleventyConfig.addPassthroughCopy("src/_scripts/**/*.js");
+    //eleventyConfig.addPassthroughCopy("src/_styles/**/*.css");
+    //eleventyConfig.addPassthroughCopy("src/_scripts/**/*.js");
     eleventyConfig.addPassthroughCopy("src/_images/**/*.jpg");
     eleventyConfig.addPassthroughCopy("src/_images/**/*.png");
 
