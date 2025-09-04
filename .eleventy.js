fccf0a2204ace97cc2b1d93e3b2c7215ddf10990
Ian Viveiros <iviveiros3@gatech.edu>
2025-09-04 19:47:11 -0400

    Update package.json to be correct

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,6 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/_images/**/*.jpg");
     eleventyConfig.addPassthroughCopy("src/_images/**/*.png");
-    eleventyConfig.addPassthroughCopy("src/_videos/**/*.mp4");
 
     return {
         dir: {
